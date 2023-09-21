9a88186ba43177b5f9735decac9d098dc9b4132d
giftojcs <ojaighog@gmail.com>
2023-09-20 21:09:53 -0600

    initial commit

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,27 @@
-import React from "react";
+import React from 'react';
+import '../styles/PhotoListItem.scss';
 
-import "../styles/PhotoListItem.scss";
+const PhotoListItem = (props) => {
+  const { username, imageSource, id, location, profile } = props;
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+  return (
+    <div className="photo-list-item"> {/* Apply the class for styling */}
+      <img src={imageSource} alt={`Photo ${id}`} />
+      <div className="photo-details">
+        <div className="profile">
+          <div className="profile-image">
+            <img src={profile} alt={`Profile ${username}`} />
+          </div>
+          <div className="profile-info">
+            <span>{username}</span>
+          </div>
+        </div>
+        <div className="location">{`${location.city}, ${location.country}`}</div>
+      </div>
+    </div>
+  );
 };
 
 export default PhotoListItem;
+
+
