10e1d802c9f6cae538487063eaf46739b61e21d2
giftojcs <ojaighog@gmail.com>
2023-09-24 16:35:34 -0600

    updating code for TopicListItem

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -1,15 +1,27 @@
 import React from "react";
+import { FaHeart } from "react-icons/fa";
 import "../styles/TopicListItem.scss";
 
-const TopicListItem = ({ topic }) => {
-  const { id, slug, label } = topic;
+const TopicListItem = ({ data }) => {
+  const { id, slug, title, user } = data;
 
   return (
-    <div className="topic-list__item" key={id}>
-      <span className="topic-label">{label}</span>
+    <div className="topic-list-item">
+      <div className="user-profile">
+        {user && user.profile && <img src={user.profile} alt={`${user.username}'s profile`} className="user-profile-image" />}
+        {user && user.username && <span className="username">{user.username}</span>}
+      </div>
+      <img src="path/to/image.jpg" alt={title} className="topic-image" />
+      <div className="topic-details">
+        <p>ID: {id}</p>
+        <p>Slug: {slug}</p>
+        <p>Title: {title}</p>
+      </div>
+      <div className="like-button">
+        <FaHeart />
+      </div>
     </div>
   );
 };
 
 export default TopicListItem;
-
