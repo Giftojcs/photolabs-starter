3ef87a2e494ccd5b2f754bd15870cc35680a7992
giftojcs <ojaighog@gmail.com>
2023-09-24 16:49:52 -0600

    updating code for App file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,70 +1,29 @@
-import React from 'react';
-import PhotoListItem from './components/PhotoListItem';
-import PhotoList from './components/PhotoList';
-import TopicListItem from "./components/TopicListItem";
-import TopNavigationBar from "./components/TopNavigationBar";
-
+import React, { useState } from 'react';
+import HomeRoute from './routes/HomeRoute';
+import PhotoDetailsModal from './routes/PhotoDetailsModal';
+import useApplicationData from './Hooks/useApplicationData';
+import TopicListItem from './components/TopicListItem';
 import './App.scss';
 
-/*const photoData = [
-  {
-    id: "1",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    username: "Joe Example",
-    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-  },
-  {
-    id: "2",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`,
-    username: "Joe Example",
-    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-  },
-  {
-    id: "3",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`,
-    username: "Joe Example",
-    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-  },
-];*/
-
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    label: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    label: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    label: "People",
-  },
-];
-
 const App = () => {
-  const likedCount = 5;
-  const photoCategories = ["topic-1", "topic-2", "topic-3"]; 
+  const { state } = useApplicationData();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleToggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+    console.log('isModalOpen:', !isModalOpen);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    console.log('isModalOpen:', false);
+  };
 
   return (
     <div className="App">
-      <TopNavigationBar likedCount={likedCount} photoCategories={photoCategories} />
-      {/* Other components and content of your app */}
+      {/* Pass state.photoData to HomeRoute */}
+      <HomeRoute handleToggleModal={handleToggleModal} photoData={state?.photoData} />
+      {isModalOpen && <PhotoDetailsModal handleCloseModal={handleCloseModal} />}
     </div>
   );
 };
