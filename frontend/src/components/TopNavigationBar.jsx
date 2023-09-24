56856d04743b19b6d23c4153ba744f26eaa12475
giftojcs <ojaighog@gmail.com>
2023-09-24 16:36:30 -0600

    updating code for TopNavigationBar

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
-import '../styles/TopNavigationBar.scss'
-import Logo from "./AppLogo";
-import TopicList from "./TopicList";
-//import LikedPhotos from "./LikedPhotos";
-import { FaHeart } from "react-icons/fa";
+import '../styles/TopNavigationBar.scss';
+import { FaHeart } from 'react-icons/fa';
 
-const TopNavigationBar = ({ likedCount, photoCategories }) => {
+const TopNavigationBar = ({ likedCount, topic, onTopClick, favorites }) => {
+  const notificationCount = favorites.length;
+
   return (
     <div className="top-nav-bar">
-      <Logo />
-      <TopicList photoCategories={photoCategories} />
+      <div className="logo">
+        {/* Placeholder for logo */}
+        PHOTOLABS
+      </div>
+
+      <div className="topics">
+        <div className="topic">Nature</div>
+        <div className="topic">Travel</div>
+        <div className="topic">People</div>
+        <div className="topic">Fashion</div>
+        <div className="topic">Animals</div>
+      </div>
+
       <div className="liked-photos">
         <FaHeart /> Total liked photos: {likedCount}
+      </div>
+
+      <div className="heart-icon">
+        <i className="fa fa-heart"></i>
+        {notificationCount > 0 && (
+          <div className="notification-count">{notificationCount}</div>
+        )}
       </div>
     </div>
   );
 };
 
 export default TopNavigationBar;
+
