8d8b3bbe4cdaa3a9c80626adb83a8ece473dd99a
giftojcs <ojaighog@gmail.com>
2023-09-24 16:42:24 -0600

    updating code for HomeRoutes

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,73 +1,12 @@
-import React, { useState } from "react";
-import TopNavigationBar from "../components/TopNavigationBar";
-import PhotoList from "../components/PhotoList";
-import "../styles/HomeRoute.scss";
+import React, { useState } from 'react';
+import TopNavigationBar from '../components/TopNavigationBar';
+import PhotoList from '../components/PhotoList';
+import TopicList from '../components/TopicList';
+import '../styles/HomeRoute.scss';
+import useApplicationData from '../Hooks/useApplicationData';
 
-  const toggleFavorite = (photoId) => {
-    setFavorites((prevFavorites) => {
-      if (prevFavorites.includes(photoId)) {
-        return prevFavorites.filter((id) => id !== photoId);
-      } else {
-        return [...prevFavorites, photoId];
-      }
-    });
-  };
-
-const sampleDataForPhotoList = [
-  {
-    id: "1",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-1-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    },
-    user: {
-      id: "1",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-  },
-  {
-    id: "2",
-    location: {
-      city: "Toronto",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-2-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`,
-    },
-    user: {
-      id: "2",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-  },
-  {
-    id: "3",
-    location: {
-      city: "Ottawa",
-      country: "Canada",
-    },
-    urls: {
-      full: `${process.env.PUBLIC_URL}/Image-3-Full.jpeg`,
-      regular: `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`,
-    },
-    user: {
-      id: "3",
-      username: "exampleuser",
-      name: "Joe Example",
-      profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-    },
-  },
-];
-
-const HomeRoute = () => {
+const HomeRoute = ({ handleToggleModal, photoData, topicData }) => {
+  const { state, fetchPhotosByTopic } = useApplicationData();
   const [favorites, setFavorites] = useState([]);
 
   const toggleFavorite = (photoId) => {
@@ -80,20 +19,24 @@ const HomeRoute = () => {
     });
   };
 
-  const handleToggleModal = (photoId) => {
-    // Handle toggle modal for the specific photo
-    console.log('Toggle modal for photo with id:', photoId);
+  const handleTopicClick = (topicId) => {
+    fetchPhotosByTopic(topicId); // Fetch photos for the clicked topic
   };
 
-  return (
+return (
     <div className="home-route">
-      <TopNavigationBar favorites={favorites} />
+      <TopNavigationBar
+        favorites={favorites}
+        topics={topicData} // Pass topics to the TopNavigationBar
+        onTopicClick={handleTopicClick} // Pass the topic click handler
+      />
       <PhotoList
-        photoData={photoData} // Pass the photo data to the PhotoList
+      photos={state.photoData}
         favorites={favorites}
         toggleFavorite={toggleFavorite}
-        handleToggleModal={handleToggleModal} // Pass the toggle function
+        handleToggleModal={handleToggleModal}
       />
+      <TopicList topicData={topicData} />
     </div>
   );
 };
