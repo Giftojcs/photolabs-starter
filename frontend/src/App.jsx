4e33812ca1c1cc7090180959c3b5e6715d186a53
giftojcs <ojaighog@gmail.com>
2023-10-05 19:27:12 -0600

    Making changes to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
-
 import './App.scss';
-
 import HomeRoute from 'routes/HomeRoute';
 import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 import useApplicationData from 'hooks/useApplicationData';
 
-// Note: Rendering a single component to build components in isolation
+// Note: Rendering a each component to build components in isolation
 const App = () => {
 
   const { state, updateToFavPhotoIds, setPhotoSelected, onClosePhotoDetailsModal, getPhotosByTopic } = useApplicationData();
